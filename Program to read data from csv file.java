1f188c0736d85cf8d90eb14cc2909472923d1779
Likith Shet <96128274+shet27@users.noreply.github.com>
2023-04-12 21:31:43 +0530

    Create Program to read data from csv file.java

---
+++ Program to read data from csv file.java
@@ -0,0 +1,15 @@
+
+import java.io.*;
+import java.util.Scanner;
+
+public class csv {
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(new File("file location"));
+        sc.useDelimiter(",");
+        while (sc.hasNext()) {
+            System.out.print(sc.next());
+        }
+        sc.close();
+    }
+}
